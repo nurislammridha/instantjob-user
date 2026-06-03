5b86985512c0c3b6ed38a8ec0cba0e20a9f31f66
nurislammridha <mridha851@gmail.com>
2026-06-03 11:19:08 +0600

    user app

--- assets/functions/env.js
+++ assets/functions/env.js
@@ -2,5 +2,5 @@
 // For physical device, replace with your machine's local IP (e.g. http://192.168.1.x:5000)
 
 // export const rootUrl = "http://192.168.0.108:8086/v1/api/";
-// export const rootUrl = "https://instantjob-api.onrender.com/v1/api/";
-export const rootUrl = "http://192.168.0.108:8086/v1/api/";
+export const rootUrl = "https://instantjob-api.onrender.com/v1/api/";
+// export const rootUrl = "http://192.168.0.108:8086/v1/api/";
